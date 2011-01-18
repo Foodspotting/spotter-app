ed307854dd809abf69a092218fe130587c4d2122
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-01-19 00:45:26 +0900

    OAuth2 works!
    - Logging in works
    - Getting the user works
    - Getting recent and wanted sightings works
    - Still need to verify that spotting works

--- simple_spotter.rb
+++ simple_spotter.rb
@@ -3,7 +3,6 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 require 'uri'
-require 'oauth'
 require 'cgi'
 require 'pp'
 
@@ -11,14 +10,65 @@ enable :sessions
 
 API_KEY = ''
 API_SECRET = ''
+REDIRECT_URI = 'http://localhost:4567/callback'
+
+module Tubes
+  def self.get(url, params = {}, headers = {})
+    if params.keys.size > 0
+      url = "#{url}?" + params.collect { |k,v| "#{k}=#{v}" } * '&'
+    end
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.request_uri, headers)
+    http.request(req)
+  end
+
+  def self.post(url, params = {}, data = nil, headers = {})
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.request_uri, headers)
+    if data.nil?
+      req.set_form_data(params)
+    else
+      req.set_form_data(data)
+    end
+    http.request(req)
+  end
+end
+
+class OAuth2
+  class Consumer
+    def initialize(key, secret, provider_url, redirect_uri)
+      @key = key
+      @secret = secret
+      @provider_url = provider_url
+      @redirect_uri = redirect_uri
+    end
+
+    def authorization_url
+      "#{@provider_url}/oauth/authorize?response_type=code&client_id=#{@key}&redirect_uri=#{@redirect_uri}"
+    end
+
+    def get_access_token(auth_code)
+      res = Tubes::post("#{@provider_url}/oauth/token",
+                        {'grant_type' => 'authorization_code',
+                         'code' => auth_code,
+                         'redirect_uri' => REDIRECT_URI,
+                         'client_id' => @key,
+                         'client_secret' => @secret})
+      data = JSON.parse(res.body)
+      data['access_token']
+    end
+  end
+end
 
 before do
-  @consumer = OAuth::Consumer.new(API_KEY, API_SECRET, :site => Foodspotting::SITE_URL)
+  @consumer = OAuth2::Consumer.new(API_KEY, API_SECRET, Foodspotting::SITE_URL, REDIRECT_URI)
 end
 
 get '/' do
   @sightings = Foodspotting.recent_sightings || []
-  
+
   if session['access_token']
     @wanted = Foodspotting.wanted_sightings(session['access_token'])
   else
@@ -34,8 +84,7 @@ post '/spot' do
 end
 
 get '/login' do
-  session['request_token'] = @consumer.get_request_token
-  redirect session['request_token'].authorize_url
+  redirect @consumer.authorization_url
 end
 
 get '/logout' do
@@ -45,8 +94,7 @@ get '/logout' do
 end
 
 get '/callback' do
-  session['access_token'] = session['request_token'].get_access_token(:oauth_verifier => params['oauth_verifier'])
-  session['request_token'] = nil
+  session['access_token'] = @consumer.get_access_token(params[:code])
   session['user'] = Foodspotting.logged_in_user(session['access_token'])
   redirect '/'
 end
@@ -56,34 +104,23 @@ class Foodspotting
   API_URL = 'http://www.foodspotting.com/api'
 
   def self.recent_sightings
-    uri = URI.parse(api_url('/sightings'))
-    res = Net::HTTP.get_response(uri)
+    res = Tubes::get("#{API_URL}/sightings", {'api_key' => API_KEY})
     JSON.parse(res.body)
   end
 
   def self.logged_in_user(access_token)
-    uri = api_url('/people/current')
-    res = access_token.get(uri)
+    res = Tubes::get("#{API_URL}/people/current", {'api_key' => API_KEY, 'oauth_token' => access_token})
     JSON.parse(res.body)
   end
 
   def self.wanted_sightings(access_token)
-    uri = api_url('/sightings', {:filter => :wanted})
-    res = access_token.get(uri)
+    res = Tubes::get("#{API_URL}/sightings", {'filter' => 'wanted', 'api_key' => API_KEY, 'oauth_token' => access_token})
     JSON.parse(res.body)
   end
 
   def self.spot(post_body, access_token, content_type)
-    uri = api_url('/reviews')
-    res = access_token.post(uri, post_body, {'Content-Type' => content_type})
+    res = Tubes::post("#{API_URL}/reviews", {'api_key' => API_KEY, 'oauth_token' => access_token},
+                      post_body, {'Content-Type' => content_type})
     JSON.parse(res.body)
   end
-
-  private
-
-  def self.api_url(path, params = {})
-    params = {:api_key => API_KEY}.merge(params)
-    "#{API_URL}#{path}.json?" + (params.collect { |k, v| "#{k}=#{CGI::escape(v.to_s)}" } * '&')
-  end
-
 end
